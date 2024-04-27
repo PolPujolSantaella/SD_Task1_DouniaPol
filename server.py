2fc3ea97a1d91a785bfab0271fe7ade414cb03f0
Douuniia22 <102051122+Douuniia22@users.noreply.github.com>
2024-04-28 00:14:18 +0200

    Update server.py

--- server.py
+++ server.py
@@ -12,24 +12,47 @@ class ChatService(chat_pb2_grpc.ChatServiceServicer):
         username = request.username
         chat_id = request.chat_id
 
+
         ip_address, port = self.name_server.get_user_address(chat_id)
 
         if ip_address and port:
             return chat_pb2.ConnectionResponse(connected=True, ip_address=ip_address, port=port)
         else:
             return chat_pb2.ConnectionResponse(connected=False, ip_address="", port=0)
+    def SendMessage(self, request, context):
+        sender = request.sender
+        recipient = request.recipient
+        message = request.message
+
+        ip_address, port = self.name_server.get_user_address(recipient)
+        
+        if ip_address and port:
+            print(f"Mensaje enviado de {sender} a {recipient} en {ip_address}:{port}: {message}")
+            return chat_pb2.MessageResponse(success=True)
+        else:
+            return chat_pb2.MessageResponse(success=False)
+
 
 class NameServer:
     def __init__(self):
         self.redis_client = redis.StrictRedis(host="localhost", port=6379, db=0)
 
     def register_user(self, username, ip_address, port):
-        self.redis_client.hmset(username, {'ip_address': ip_address, 'port': port})
+        user_info = f"{ip_address}:{port}"
+        self.redis_client.set(username, user_info)
+
 
     def get_user_address(self, username):
-        user_data = self.redis_client.hgetall(username)
+        user_data = self.redis_client.get(username)
         if user_data:
-            return user_data.get(b'ip_address').decode(), int(user_data.get(b'port').decode())
+
+
+            user_info = user_data.decode()
+            ip_address, port_str = user_info.split(':')
+            port = int(port_str)
+            print(ip_address)
+            print(port)
+            return ip_address, port
         else:
             return None, None
 
