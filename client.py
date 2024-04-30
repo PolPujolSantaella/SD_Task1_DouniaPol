dbc89216873216d2e3a1774acc4751638c73d1b7
PolPujolSantaella <pol.pujol.santaella@gmail.com>
2024-04-30 11:58:21 +0200

    Inici Message Broker

--- client.py
+++ client.py
@@ -26,7 +26,7 @@ class ChatClient(chat_pb2_grpc.ChatClientServicer):
         request = chat_pb2.ChatRequest(username=self.username, chat_id=chat_id)
         response = self.server_stub.Connect(request)
         if response.success:
-            print(f"Conectat al chat {chat_id} a {response.ip}:{response.port}")
+            print(f"\t CHAT {chat_id}")
             self.user_channel = grpc.insecure_channel(f'{response.ip}:{response.port}')
             self.user_stub = chat_pb2_grpc.ChatClientStub(self.user_channel)
             return response.ip, response.port
@@ -34,41 +34,52 @@ class ChatClient(chat_pb2_grpc.ChatClientServicer):
             print(f"No s'ha pogut conectar amb {chat_id}")
             return None
 
-    def send_message(self, message):
+    def send_messages(self, message, username):
         request = chat_pb2.Message(sender=self.username, message=message)
         response = self.user_stub.ReceiveMessage(request)
+        if not response.success:
+            print("El destinatari no està conectat al chat.")
 
     def ReceiveMessage(self, request, context):
         print (f"{request.sender}: {request.message}")
         return chat_pb2.MessageResponse(success=True)
 
     def start_server(self, client, port_client):
-        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
         chat_pb2_grpc.add_ChatClientServicer_to_server(client, server)
         server.add_insecure_port(f'[::]:{port_client}')
         server.start()
         try:
+            print ("(Per sortir Ctrl+C).")
             while True:
-                message = input("")
-                client.send_message(message)
+                    message = input("")
+                    client.send_messages(message, self.username)
         except KeyboardInterrupt:
             server.stop(0)
 
+   # def subscribe_to_group_chat(self, chat_id):
+
+
 if __name__ == "__main__":
-    username = input("Ingrese su nombre de usuario: ")
+    username = input("Login: ")
     client = ChatClient(username)
     port_client = client.login()
     if port_client:
         while True:
             print("\n1. Connect To Chat")
-            print("2. Salir")
+            print("2. Subscribe to chat")
+            print("3. Salir")
             choice = input("Seleccione una opción: ")
 
             if choice == "1":
-                chat_id = input("Ingrese el nombre de usuario del destinatario: ")
+                chat_id = input("Chat al que vols connectar: ")
                 client.connect_to_chat(chat_id)
                 client.start_server(client, port_client)
             elif choice == "2":
+                chat_id = input("Grup al que vols subscriure't : ")
+                #client.subscribe_to_group_chat(chat_id)
+                break
+            elif choice == "3":
                 break
             else:
                 print("Opción inválida. Intente de nuevo.")
--- server.py
+++ server.py
@@ -4,10 +4,12 @@ import chat_pb2_grpc
 import redis
 from concurrent import futures
 import time
+import pika
 
 class ChatService(chat_pb2_grpc.ChatServiceServicer):
-    def __init__(self, name_server):
-        self.name_server = name_server
+    def __init__(self):
+        self.name_server = NameServer()
+        self.message_broker = MessageBroker()
 
     def Login(self, request, context):
         username = request.username
@@ -18,6 +20,7 @@ class ChatService(chat_pb2_grpc.ChatServiceServicer):
             return chat_pb2.Response(success=True, message="Usuari Registrat", ip=ip, port=int(port))
         else:
             return chat_pb2.Response(success=False, message="Usuari no registrat")
+
     def Connect(self, request, context):
         chat_id = request.chat_id
         chat_address = self.name_server.get_user_address(chat_id)
@@ -33,17 +36,29 @@ class NameServer:
             self.redis_client = redis.StrictRedis(host="localhost", port=6379, password="", decode_responses=True)
             self.redis_client.set("usr:Pepe", "127.0.0.1:50052")
             self.redis_client.set("usr:Anna", "127.0.0.1:50053")
-
+            self.redis_client.set("usr:Dounia", "127.0.0.1:50054")
+            self.redis_client.set("usr:Pol", "127.0.0.1:50055")
         except Exception as e:
             print(e)
 
     def get_user_address(self, user):
         return self.redis_client.get(f"usr:{user}")
 
+
+class MessageBroker:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange='group_chat', exchange_type='fanout')
+
+    def publish_message(self, message):
+        self.channel.basic_publish(exchange='group_chat', routing_key='', body=message)
+
+
+
 def run_server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    name_server = NameServer()
-    chat_pb2_grpc.add_ChatServiceServicer_to_server(ChatService(name_server), server)
+    chat_pb2_grpc.add_ChatServiceServicer_to_server(ChatService(), server)
     print('Starting server. Listening on port 50051.')
     server.add_insecure_port('0.0.0.0:50051')
     server.start()
@@ -54,5 +69,4 @@ def run_server():
         server.stop(0)
 
 if __name__ == '__main__':
-    name_server = NameServer()
     run_server()
